b5d1d752f8f99bfe30f0426decada23718150132
Metin Mustafa Altıntaş <149479684+MetinMustafaAltintas@users.noreply.github.com>
2024-08-06 16:54:59 +0300

    Onion architecture alt yapısı oluşturuldu

---
+++ Infastructure/ECommerceAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            => new() 
+            {
+                new Product(){  Id= Guid.NewGuid(),Name = "Product1",Price = 100,Stock =10},            
+                new Product(){  Id= Guid.NewGuid(),Name = "Product2",Price = 200,Stock =10},           
+                new Product(){  Id= Guid.NewGuid(),Name = "Product3",Price = 300,Stock =10},            
+                new Product(){  Id= Guid.NewGuid(),Name = "Product4",Price = 400,Stock =10},           
+                new Product(){  Id= Guid.NewGuid(),Name = "Product5",Price = 500,Stock =10}            
+            };
+    }
+}
