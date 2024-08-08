5fbd019b83a44cc17532b83905d14fcadd331f27
Metin Mustafa Altıntaş <149479684+MetinMustafaAltintas@users.noreply.github.com>
2024-08-08 19:54:27 +0300

    Tracking ayarlaması ve Repositorydeki methodlarda düzektme yapıldı

--- Presentation/ECommerceAPI.API/Controllers/ProductController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,16 +18,27 @@ namespace ECommerceAPI.API.Controllers
             _productReadRepository = productReadRepository;
         }
         [HttpGet]
-        public async void Get() 
+        public async Task Get() 
         {
-            _productWriteRepository.AddRangeAsync(new()
-            {
-                new() { Id=Guid.NewGuid(), Name="Product1",Price=100,CreatedDate=DateTime.UtcNow,Stock = 10},
-                new() { Id=Guid.NewGuid(), Name="Product2",Price=200,CreatedDate=DateTime.UtcNow,Stock = 20},
-                new() { Id=Guid.NewGuid(), Name="Product3",Price=300,CreatedDate=DateTime.UtcNow,Stock = 30},
-            });
+            //_productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() { Id=Guid.NewGuid(), Name="Product1",Price=100,CreatedDate=DateTime.UtcNow,Stock = 10},
+            //    new() { Id=Guid.NewGuid(), Name="Product2",Price=200,CreatedDate=DateTime.UtcNow,Stock = 20},
+            //    new() { Id=Guid.NewGuid(), Name="Product3",Price=300,CreatedDate=DateTime.UtcNow,Stock = 30},
+            //});
+            //await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("8e6317bd-683e-4b03-81f9-9179c158ba60",false);
+            p.Name = "Mehmet";
             await _productWriteRepository.SaveAsync();
         }
 
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> Get(string id)
+        {
+          Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
+        }
     }
 }
